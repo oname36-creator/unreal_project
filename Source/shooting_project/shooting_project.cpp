c545ad350947006569cfdde0df781d1007a8705a
98 <oname36@gmail.com>
2026-08-27 10:36:13 +0900

    Project init

---
+++ Source/shooting_project/shooting_project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "shooting_project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, shooting_project, "shooting_project" );
